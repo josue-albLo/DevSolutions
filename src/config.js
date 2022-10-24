8fcc36a058062cc62b12306a0637eff00d67440b
Josue Guevara <guevarajosue637@gmail.com>
2022-10-23 23:22:29 -0600

    mas correciones

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ export default{
     port_server: Number(process.env.PORT_SERVER) || 4000,
     port_database: Number(process.env.PORT_DATABASE),
     server: String(process.env.SERVER),
-    database: String(process.env.DATABASE),
-    user:String(process.env.USER),
-    password: String(process.env.PASSWORD)
+    database: process.env.DATABASE,
+    user:process.env.USER,
+    password: process.env.PASSWORD
 
 }

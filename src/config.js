ccf41bff427e6886fd62f6dcf71435c25578fefc
Josue Guevara <guevarajosue637@gmail.com>
2022-10-23 22:49:59 -0600

    variables actualizadas

--- src/config.js
+++ src/config.js
@@ -9,9 +9,9 @@ config();
 export default{
     port_server: Number(process.env.PORT_SERVER) || 4000,
     port_database: Number(process.env.PORT_DATABASE),
-    server: process.env.SERVER,
-    database: process.env.DATABASE,
-    user:process.env.USER,
-    password: process.env.PASSWORD
+    server: String(process.env.SERVER),
+    database: String(process.env.DATABASE),
+    user:String(process.env.USER),
+    password: String(process.env.PASSWORD)
 
 }

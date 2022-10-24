cc3d851977128ec6a57920385feca73ce872d955
Josue Guevara <guevarajosue637@gmail.com>
2022-10-23 23:33:28 -0600

    modifcado las configs

--- src/database/conection.js
+++ src/database/conection.js
@@ -9,7 +9,7 @@ const dbSettings ={
     port: config.port_database,
     options:{
         encrypt: true, // for azure
-        trustServerCertificate: false // change to true for local dev / self-signed certs
+        trustServerCertificate: true // change to true for local dev / self-signed certs
     }
 
 }
